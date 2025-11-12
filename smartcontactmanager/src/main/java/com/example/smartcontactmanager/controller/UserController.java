22252ffd46cea8198602b11167116dcd45d15d3c
akshayshahasane <akshayshahasane2527@gmail.com>
2025-11-12 12:21:09 +0530

    commit13

--- smartcontactmanager/src/main/java/com/example/smartcontactmanager/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/example/smartcontactmanager/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -27,6 +28,9 @@ import java.util.Optional;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -282,5 +286,41 @@ public class UserController {
     }
 
 
+    //open settings handler
+
+    @GetMapping("/settings")
+    public String openSettings(){
+        return "normal/settings";
+    }
+
+    //change password handler
+
+    @PostMapping("/change-password")
+    public String changePassword(@RequestParam("oldPassword")String oldPassword, @RequestParam("newPassword")String newPassword, Principal principal, HttpSession session) {
+
+        System.out.println("OLD PASSWORD "+oldPassword);
+        System.out.println("NEW PASSWORD "+newPassword);
+
+        String userName = principal.getName();
+        User currentUser = this.userRepository.getUserByUserName(userName);
+        System.out.println(currentUser.getPassword());
+
+        if (this.bCryptPasswordEncoder.matches(oldPassword, currentUser.getPassword())) {
+
+            //change the password
+
+            currentUser.setPassword(this.bCryptPasswordEncoder.encode(newPassword));
+            this.userRepository.save(currentUser);
+            session.setAttribute("message", new Message("Password changed successfully!", "success"));
+        }else {
+
+            //error
+
+            session.setAttribute("message", new Message("Wrong password!", "danger"));
+            return "redirect:/user/settings";
+        }
+
+        return "redirect:/user/index";
+    }
 
 }
